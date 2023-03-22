fde1ef46377281f91a46cfdfd55098c34d6c4c13
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-03-22 14:32:00 -0400

    second commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,57 +1,75 @@
-import React from "react";
+import React, { isValidElement } from "react";
 import { useState } from "react";
 import './Home.css'
 
 
 
 export default function Home(){
-    const [planetName, setPlanetName] = useState([
+    const [Planets, setPlanets] = useState([
         {
             name: "Earth",
             funfact: "Did you know earth is 70% water?",
-            img: "url"
+            img: "url",
+            id: 0
             
         },
         {
             name: "Mars",
-            description: "This is mars, woooww",
-            img: "url"
+            funfact: "This is mars, woooww",
+            img: "url",
+            id: 1 
             
         },
         {
             name: "jupiter",
-            description: "This is mars, woooww",
-            img: "url"
+            funfact: "This is mars, woooww",
+            img: "url",
+            id: 2
             
         },
         {
             name: "Saturn",
-            description: "This is saturn, woooww",
-            img: "url"
+            funfact: "This is saturn, woooww",
+            img: "url",
+            id: 3
+
             
         }
     ]);
-    const [description, setDescription] = useState(['something something '])
+    // const [description, setDescription] = useState(['something something '])
 
-    function changePlanetName(params) {
-        setPlanetName('Earth')
-        
+    function changePlanetName() {
+       
+            <div>
+                 <ul>
+                      
+             {Planets.map((planet) => 
+                <li key={planet.id}>
+                {planet.id}
+                </li>
+                )}
+            </ul>
+            {console.log(Planets[1])}
+            </div>
+           
+    
     }
 
     function changeDescription(){
-        setDescription('something else something else')
+        // setDescription('something else something else')
     }
 
 
     return(
         <div className="Home">
+            
             <h1>ADVENTURES WITH SPACE INVADERS</h1>
-            <h2>{planetName}</h2>
-            <p>{description}</p>
+            {/* <h2>{planetName}</h2> */}
+            {/* <p>{description}</p> */}
             <button onClick={()=> {
                 
                 changePlanetName()
-                changeDescription()
+                
             
             } } >Click to View Planets</button>
         </div>
